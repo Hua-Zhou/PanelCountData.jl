e453adff494c5e30bfbe83c1534758dcaaf296f0
Hua Zhou <huazhou@ucla.edu>
2021-10-14 12:56:28 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.18

---
+++ src/PanelCountData.jl
@@ -0,0 +1,5 @@
+module PanelCountData
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PanelCountData
+using Test
+
+@testset "PanelCountData.jl" begin
+    # Write your tests here.
+end
