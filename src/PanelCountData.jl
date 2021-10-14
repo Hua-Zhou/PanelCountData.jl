cf9ba0e33e3f59d3b10541089eba40a378545f24
Hua Zhou <huazhou@ucla.edu>
2021-10-14 13:07:41 -0700

    add welcome function

--- src/PanelCountData.jl
+++ src/PanelCountData.jl
@@ -1,5 +1,10 @@
 module PanelCountData
 
+export welcome
+
 # Write your package code here.
+function welcome()
+    println("welcome to Panel Count.")
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "PanelCountData.jl" begin
     # Write your tests here.
+    @test welcome() == "welcome to Panel Count."
 end
